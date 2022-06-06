bcf1e2594659c421173c51bbbc9550f23fcb85f4
João Ricardo Lemes Polachin <joao.polachin@programmers.com.br>
2022-06-06 15:35:59 -0300

    Exercicios Faculdade

---
+++ ativFacul/Program.cs
@@ -0,0 +1,17 @@
+﻿double N;
+
+Console.Write("Digite a sua nota: ");
+N=double.Parse(Console.ReadLine());
+
+if (N<5)
+{
+    Console.WriteLine("Aluno reprovado");
+}
+if (N>=5 && N<7)
+{
+    Console.WriteLine("Aluno de recuperação");
+}
+if (N>=7)
+{
+    Console.WriteLine("Aluno aprovado");
+}
---
+++ ativFacul2/Program.cs
@@ -0,0 +1,8 @@
+﻿double N;
+
+Console.Write("Digite a sua nota: ");
+N = double.Parse(Console.ReadLine());
+
+var result = N > 5 ? "Aluno aprovado" : "Aluno reprovado";
+Console.WriteLine(result);
+
